a135d965a4ea7e9f039a6d4ad522c80db3e5a3ed
H Grasso: PC <aitch.grasso@gmail.com>
2024-05-27 20:38:25 -0500

    padding added to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     
-    <main className="bg-zinc-800 justify-center flex min-h-full flex-col items-center">
+    <main className="bg-zinc-800 justify-center flex min-h-full flex-col items-center p-4 gap-5">
       
-      <div className=" sm:w-1/4 w-full h-2/3 bg-neutral-900  m-7 text-white rounded-xl shadow-2xl">
+      <div className=" sm:w-1/4 w-full h-2/3 bg-neutral-900 text-white rounded-xl shadow-2xl">
         <Image 
           src={me}
           className="rounded-t-xl bg-gradient-to-bl from-rose-400 via-fuchsia-300 to-indigo-300 h-1/6 shadow-lg" 
@@ -24,7 +24,10 @@ export default function Home() {
         </div>
         < Footer />
       </div>
-      <p>&copy; 2024 Hannah (H) Grasso. This project is licensed under the <a href="https://opensource.org/licenses/MIT">MIT License</a>. <a className="hover:text-rose-300" href="https://github.com/AitchGrasso/businesscard">Fork me?</a></p>
+      <div className="text-center">
+      <p>&copy; 2024 Hannah (H) Grasso.
+      <br/>This project is licensed under the <a href="https://opensource.org/licenses/MIT">MIT License</a>. <a className="hover:text-rose-300" href="https://github.com/AitchGrasso/businesscard">Fork me?</a></p>
+      </div>
       
     </main>
   );
